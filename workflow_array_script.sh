1f0bbffda58f3b907830b6f9f14b6074ce9af065
emittman <emittman@iastate.edu>
2017-05-21 12:04:23 -0500

    extend walltime

--- workflow_array_script.sh
+++ workflow_array_script.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
-#SBATCH --time=1:00:00
-#SBATCH --nodes=2 #request one node
+#SBATCH --time=3-0:00:00
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=16
 #SBATCH --partition=gpu
 #SBATCH --error=msg/array%a.err
 #SBATCH --output=msg/array%a.out
-#SBATCH --mail-user=emittman@iastate.edu   # email address
+#SBATCH --mail-user=emittman@iastate.edu
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
