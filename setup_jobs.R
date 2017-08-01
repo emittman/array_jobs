d4c50a8118a5ecb918bdb450bec75cff16806384
Eric Mittman <emittman@iastate.edu>
2017-08-01 17:47:24 -0500

    lengthen burnin period for high-dimensional chain

--- setup_jobs.R
+++ setup_jobs.R
@@ -8,7 +8,7 @@ sapply(ff, dir.create, showWarnings=FALSE)
 # Instructions for dataset simulation
 datasets = commands(
   small = setup_sim(5000, 1000, 4, 20000, 10000),
-  large = setup_sim(50000, 6000, 8, 20000, 10000)
+  large = setup_sim(50000, 6000, 8, 20000, 30000)
   ) %>%
   expand(values = c("rep1","rep2","rep3"))
 
