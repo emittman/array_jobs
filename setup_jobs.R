12c24bdaf97cb9022726ca3ade649c273e47d718
emittman <emittman@iastate.edu>
2017-08-06 13:59:58 -0500

    Increase K on large set

--- setup_jobs.R
+++ setup_jobs.R
@@ -7,10 +7,12 @@ sapply(ff, dir.create, showWarnings=FALSE)
 
 # Instructions for dataset simulation
 datasets = commands(
-  verysmall = setup_sim(1000, 1000, 2, 20000, 10000),
-  small = setup_sim(5000, 5000, 3, 20000, 10000),
-  medium = setup_sim(10000, 5000, 4, 20000, 10000),
-  large = setup_sim(40000, 6000, 6, 20000, 20000)) %>%
+#  verysmall = setup_sim(1000, 1000, 2, 20000, 10000),
+#  small = setup_sim(5000, 5000, 3, 20000, 10000),
+#  medium = setup_sim(10000, 5000, 4, 20000, 10000),
+  mediumv5 = setup_sim(10000, 5000, 5, 20000, 10000),
+  largek7000 = setup_sim(40000, 7000, 6, 20000, 20000),
+  largek8000 = setup_sim(40000, 8000, 6, 20000, 20000)) %>%
   expand(values = c("rep1","rep2"))
 
 # Instructions for run analysis
