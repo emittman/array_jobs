2dd5c87d0cc88dbacd35dae64e079a82b8d34600
Eric Mittman <emittman@iastate.edu>
2017-08-06 13:51:08 -0500

    Add setup of latest run

--- setup_jobs.R
+++ setup_jobs.R
@@ -7,10 +7,11 @@ sapply(ff, dir.create, showWarnings=FALSE)
 
 # Instructions for dataset simulation
 datasets = commands(
-  small = setup_sim(5000, 1000, 4, 20000, 10000),
-  large = setup_sim(50000, 6000, 8, 20000, 30000)
-  ) %>%
-  expand(values = c("rep1","rep2","rep3"))
+  verysmall = setup_sim(1000, 1000, 2, 20000, 10000),
+  small = setup_sim(5000, 5000, 3, 20000, 10000),
+  medium = setup_sim(10000, 5000, 4, 20000, 10000),
+  large = setup_sim(40000, 6000, 6, 20000, 20000)) %>%
+  expand(values = c("rep1","rep2"))
 
 # Instructions for run analysis
 analyses = analyses(
