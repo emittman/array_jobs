97287cbbbaadb6e40cd0f4e8cdc794c59ff70276
emittman <emittman@iastate.edu>
2017-08-09 15:59:30 -0500

    source required functions

--- explore/longtest.R
+++ explore/longtest.R
@@ -1,4 +1,8 @@
 #single long job "highest settings", i.e. G=40000, V=6, K=8000
+library(cudarpackage)
+
+source("../my_code.R")
+
 setup <- setup_sim(40000, 8000, 6, 120000, 1)
 
 dir.create(file.path("misc"))
