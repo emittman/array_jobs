433165b709c13952c455a31d38accdeee508380f
emittman <emittman@iastate.edu>
2017-08-10 14:26:37 -0500

    Add test script for summarizing analyses

---
+++ summaries/test_summaries.R
@@ -0,0 +1,13 @@
+#working directory is assumed to be array_jobs/summaries/
+
+dir.create(file.path("data"))
+
+library(plyr)
+df <- ldply(c("sss","ssm","ssl","sms","smm","sml"), function(f) {
+  file <- paste("../analyses/", "mc_sb_", f, ".rds", sep = "")
+  dat <- readRDS(dat)
+  len <- length(dat[[1]][["alpha"]])
+  data.frame(which.sim=f, t(summary(dat[[1]][["alpha"]][10000:len])))
+})
+
+saveRDS(df, "summaries_alpha.rds")
