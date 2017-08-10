1c62b8824f6471d177fe020fdc810e560de4b3f3
emittman <emittman@iastate.edu>
2017-08-10 15:48:34 -0500

    Return result

--- summaries/test_summaries.R
+++ summaries/test_summaries.R
@@ -12,7 +12,8 @@ df <- ldply(c("sss","ssm","ssl","sms","smm","sml"), function(f) {
   d <- data.frame(which.sim=f, t(as.numeric(summary(dat[[1]][["alpha"]][20000:len]))),
                   effectiveSize(mcmc(dat[[1]][["alpha"]][20000:len])),
                   dat[[4]])
-  names(d)[2:7] <- c("min","q1","median","mean","q3","max","n_eff","time")
+  names(d)[2:9] <- c("min","q1","median","mean","q3","max","n_eff","time")
+  d
 })
 
 saveRDS(df, "summaries_alpha.rds")
