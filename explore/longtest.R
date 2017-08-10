a8b1fdfaeda5e086f4ec4340a8dc05efc1e21869
emittman <emittman@iastate.edu>
2017-08-10 15:43:50 -0500

    Test reording scheme during warmup

--- explore/longtest.R
+++ explore/longtest.R
@@ -7,8 +7,35 @@ setup <- setup_sim(40000, 8000, 6, 100000, 1)
 
 dir.create(file.path("misc"))
 
-samples <- mcmc_sb(setup)
+#contr <- formatControl(n_iter=n_iter, thin=1, warmup=warmup, methodPi="stickBreaking",
+#                      idx_save=1, n_save_P=1, alpha_fixed=F, slice_width=1, max_steps=100)
 
-saveRDS(sample, "misc/longtest.rds")
+#mcmc_sb <- function(input){
+#  s <- with(input, mcmc(fdata, fpriors, control, estimates = estimates, verbose=0))
+#  return(s)
+#}
+
+warmup_cycles <- 10
+
+warmup_length <- 5000
+
+final_length <- 30000
+
+chain <- initFixedGrid(setup$fpriors, setup$estimates)
+
+for(i in warmup_cycles){
+  setup$control$n_iter <- 1
+  setup$control$warmup <- warmup_length
+  s <- with(setup, mcmc(fdata, fpriors, control, chain, estimates))
+  saveRDS(s, file=paste("misc/wrm-up-cycle-",i,".rds", sep=""))
+  zeta <- with(setup, as.integer(sample(fpriors$K, fdata$G, replace=T) - 1))
+  chain <- with(s[['state']], formatChain(beta, pi, tau2, zeta, alpha))
+}
+
+setup$control$n_iter <- final_length
+setup$control$warmup <- 1
+s <- with(setup, mcmc(fdata, fpriors, control, chain, estimates))
+saveRDS(s, "misc/final_run.rds")
+
 
   
--- summaries/test_summaries.R
+++ summaries/test_summaries.R
@@ -2,12 +2,17 @@
 
 dir.create(file.path("data"))
 
+library(coda)
 library(plyr)
+
 df <- ldply(c("sss","ssm","ssl","sms","smm","sml"), function(f) {
   file <- paste("../analyses/", "mc_sb_", f, ".rds", sep = "")
   dat <- readRDS(file)
   len <- length(dat[[1]][["alpha"]])
-  data.frame(which.sim=f, t(summary(dat[[1]][["alpha"]][10000:len])))
+  d <- data.frame(which.sim=f, t(as.numeric(summary(dat[[1]][["alpha"]][20000:len]))),
+                  effectiveSize(mcmc(dat[[1]][["alpha"]][20000:len])),
+                  dat[[4]])
+  names(d)[2:7] <- c("min","q1","median","mean","q3","max","n_eff","time")
 })
 
 saveRDS(df, "summaries_alpha.rds")
