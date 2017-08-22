c63fe09d515400b69b2a3ae30c650a5cd0853a3e
emittman <emittman@iastate.edu>
2017-08-22 11:20:35 -0500

    Run more simulations for moderate numbers of genes (8k,16k)

--- setup_jobs.R
+++ setup_jobs.R
@@ -6,26 +6,27 @@ ff <- c('datasets', 'analyses', 'msg', 'output')
 sapply(ff, dir.create, showWarnings=FALSE)
 
 # Instructions for dataset simulation
-#syntax GVK refers to G in {5000,40000}, K in {2000, 4000, 8000}, V in {2,4,6}
+#syntax GVK refers to G in 2^{12,15}, K in {2000, 4000, 8000}, V in {2,4,6}
+#edit: new letters for gene size: m = 2^13, h = 2^14
 datasets = commands(
- sss=setup_sim(G=2^12, K=2^10, V=2, n_iter=50000, warmup=5000, init.iter=500),
- ssm=setup_sim(2^12, 2^10, 4, n_iter=50000, warmup=5000, init.iter=500),
- ssl=setup_sim(2^12, 2^10, 6, n_iter=50000, warmup=5000, init.iter=500),
- sms=setup_sim(2^12, 2^11, 2, n_iter=50000, warmup=5000, init.iter=500),
- smm=setup_sim(2^12, 2^11, 4, n_iter=50000, warmup=5000, init.iter=500),
- sml=setup_sim(2^12, 2^11, 6, n_iter=50000, warmup=5000, init.iter=500),
- sls=setup_sim(2^12, 2^12, 2, n_iter=50000, warmup=5000, init.iter=500),
- slm=setup_sim(2^12, 2^12, 4, n_iter=50000, warmup=5000, init.iter=500),
- sll=setup_sim(2^12, 2^12, 6, n_iter=50000, warmup=5000, init.iter=500),
- lss=setup_sim(2^15, 2^10, 2, n_iter=50000, warmup=5000, init.iter=500),
- lsm=setup_sim(2^15, 2^10, 4, n_iter=50000, warmup=5000, init.iter=500),
- lsl=setup_sim(2^15, 2^10, 6, n_iter=50000, warmup=5000, init.iter=500),
- lms=setup_sim(2^15, 2^11, 2, n_iter=50000, warmup=5000, init.iter=500),
- lmm=setup_sim(2^15, 2^11, 4, n_iter=50000, warmup=5000, init.iter=500),
- lml=setup_sim(2^15, 2^11, 6, n_iter=50000, warmup=5000, init.iter=500),
- lls=setup_sim(2^15, 2^12, 2, n_iter=50000, warmup=5000, init.iter=500),
- llm=setup_sim(2^15, 2^12, 4, n_iter=50000, warmup=5000, init.iter=500),
- lll=setup_sim(2^15, 2^12, 6, n_iter=50000, warmup=5000, init.iter=500)
+ mss=setup_sim(G=2^13, K=2^10, V=2, n_iter=50000, warmup=5000, init.iter=500),
+ msm=setup_sim(2^13, 2^10, 4, n_iter=50000, warmup=5000, init.iter=500),
+ msl=setup_sim(2^13, 2^10, 6, n_iter=50000, warmup=5000, init.iter=500),
+ mms=setup_sim(2^13, 2^11, 2, n_iter=50000, warmup=5000, init.iter=500),
+ mmm=setup_sim(2^13, 2^11, 4, n_iter=50000, warmup=5000, init.iter=500),
+ mml=setup_sim(2^13, 2^11, 6, n_iter=50000, warmup=5000, init.iter=500),
+ mls=setup_sim(2^13, 2^12, 2, n_iter=50000, warmup=5000, init.iter=500),
+ mlm=setup_sim(2^13, 2^12, 4, n_iter=50000, warmup=5000, init.iter=500),
+ mll=setup_sim(2^13, 2^12, 6, n_iter=50000, warmup=5000, init.iter=500),
+ hss=setup_sim(2^14, 2^10, 2, n_iter=50000, warmup=5000, init.iter=500),
+ hsm=setup_sim(2^14, 2^10, 4, n_iter=50000, warmup=5000, init.iter=500),
+ hsl=setup_sim(2^14, 2^10, 6, n_iter=50000, warmup=5000, init.iter=500),
+ hms=setup_sim(2^14, 2^11, 2, n_iter=50000, warmup=5000, init.iter=500),
+ hmm=setup_sim(2^14, 2^11, 4, n_iter=50000, warmup=5000, init.iter=500),
+ hml=setup_sim(2^14, 2^11, 6, n_iter=50000, warmup=5000, init.iter=500),
+ hls=setup_sim(2^14, 2^12, 2, n_iter=50000, warmup=5000, init.iter=500),
+ hlm=setup_sim(2^14, 2^12, 4, n_iter=50000, warmup=5000, init.iter=500),
+ hll=setup_sim(2^14, 2^12, 6, n_iter=50000, warmup=5000, init.iter=500)
 )
 
 # Instructions for run analysis
--- summaries/test_summaries.R
+++ summaries/test_summaries.R
@@ -6,16 +6,72 @@ library(coda)
 library(plyr)
 
 df <- ldply(c("sss","ssm","ssl","sms","smm","sml","sls","slm","sll",
-              "lss","lsm","lsl","lms","lmm","lml","lls","llm"), function(f) {
-  file <- paste("../analyses/", "mc_sb_", f, ".rds", sep = "")
+              "lss","lsm","lsl","lms","lmm","lml","lls","llm","lll"), function(f) {
+  file <- paste("../analyses/", "mcmc_", f, ".rds", sep = "")
   dat <- readRDS(file)
   len <- length(dat[[1]][["alpha"]])
-  d <- data.frame(which.sim=f, t(as.numeric(summary(dat[[1]][["alpha"]][20000:len]))),
-                  effectiveSize(mcmc(dat[[1]][["alpha"]][20000:len])),
+  d <- data.frame(which.sim=f, #t(as.numeric(summary(dat[[1]][["alpha"]][20000:len]))),
+                  effectiveSize(mcmc(dat[[1]][["alpha"]])),
                   dat[[4]],
-                  len-20000)
-  names(d)[2:9] <- c("min","q1","median","mean","q3","max","n_eff","time","mcmc_iter")
+                  len)
+  names(d)[2:4] <- c(#"min","q1","median","mean","q3","max",
+    "n_eff","time","mcmc_iter")
   d
 })
 
 saveRDS(df, "summaries_alpha.rds")
+
+df <- ldply(c("sss","ssm","ssl","sms","smm","sml","sls","slm","sll",
+              "lss","lsm","lsl","lms","lmm","lml","lls","llm","lll"), function(f) {
+                file <- paste("../analyses/", "mcmc_", f, ".rds", sep = "")
+                dat <- readRDS(file)
+                nocc <- dat[[1]][["num_occupied"]]
+                q <- quantile(nocc, c(.05,.5,.9))
+                d <- data.frame(which.sim=f, #t(as.numeric(summary(dat[[1]][["alpha"]][20000:len]))),
+                                lower = q[1],
+                                median = q[2],
+                                upper = q[3])
+                d
+              })
+
+saveRDS(df, "summaries_num_occ.rds")
+
+df <- ldply(c("sss","ssm","ssl","sms","smm","sml","sls","slm","sll",
+              "lss","lsm","lsl","lms","lmm","lml","lls"), function(f) {
+                file <- paste("../analyses/", "mcmc2_", f, ".rds", sep = "")
+                dat <- readRDS(file)
+                len <- length(dat[[1]][["alpha"]])
+                d <- data.frame(which.sim=f, #t(as.numeric(summary(dat[[1]][["alpha"]][20000:len]))),
+                                effectiveSize(mcmc(dat[[1]][["alpha"]])),
+                                dat[[4]],
+                                len)
+                names(d)[2:4] <- c(#"min","q1","median","mean","q3","max",
+                  "n_eff","time","mcmc_iter")
+                d
+              })
+
+saveRDS(df, "summaries_alpha2.rds")
+
+df <- ldply(c("sss","ssm","ssl","sms","smm","sml","sls","slm","sll",
+              "lss","lsm","lsl","lms","lmm","lml","lls"), function(f) {
+                file <- paste("../analyses/", "mcmc2_", f, ".rds", sep = "")
+                dat <- readRDS(file)
+                nocc <- dat[[1]][["num_occupied"]]
+                q <- quantile(nocc, c(.05,.5,.9))
+                d <- data.frame(which.sim=f, #t(as.numeric(summary(dat[[1]][["alpha"]][20000:len]))),
+                                lower = q[1],
+                                median = q[2],
+                                upper = q[3])
+                d
+              })
+
+saveRDS(df, "summaries_num_occ2.rds")
+# df2 <- llply(c("sss","ssm","ssl","sms","smm","sml","sls","slm","sll",
+#                "lss","lsm","lsl","lms","lmm","lml","lls","llm"), function(f) {
+#   file <- paste("../datasets/", f, ".rds", sep = "")
+#   dat_truth <- readRDS(file)
+#   file2 <- paste("../analyses/", "mc_sb_", f, ".rds", sep = "")
+#   dat_est <- readRDS(file2)
+#   truth <- t(dat_truth)
+#   ind_estimates <- t(dat_truth$est)
+#   d <- data.frame())
